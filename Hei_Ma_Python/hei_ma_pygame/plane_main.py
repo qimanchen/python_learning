0580544bf03f21934b222116ed581531373dc64f
qimanchen <1033178199@qq.com>
2018-08-21 22:59:51 +0800

    improve plane-war

--- Hei_Ma_Python/hei_ma_pygame/plane_main.py
+++ Hei_Ma_Python/hei_ma_pygame/plane_main.py
@@ -9,7 +9,6 @@ class PlaneGame(object):
 
     def __init__(self):
         print("游戏初始化")
-
         # 1. 创建游戏的窗口
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)  # 不要把固定数值锁死
         # 2. 创建游戏时钟
@@ -17,7 +16,12 @@ class PlaneGame(object):
         # 3. 调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
         # 4. 设置定时器事件
+        # set_timer(eventid, milliseconds) -> None
+        # eventid 可以通过pygame.USEREVENT 指定每增加一个则增加1
+        # ms为set_timer的时间单位
         pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        # 设置子弹时钟
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def __create_sprites(self):
 
@@ -31,6 +35,7 @@ class PlaneGame(object):
         self.enemy_group = pygame.sprite.Group()
 
         # 创建英雄精灵
+        # 方便其他模块可以调用它
         self.hero = Hero()
         self.hero_group = pygame.sprite.Group(self.hero)
 
@@ -43,6 +48,7 @@ class PlaneGame(object):
             self.__event_handler()
             # 碰撞检测
             self.__check_collide()
+
             # 更新/绘制精灵
             self.__update_sprites()
             # 更新显示
@@ -55,12 +61,75 @@ class PlaneGame(object):
             # 判断是否退出程序
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
+            # 监测敌机创建事件实现
             elif event.type == CREATE_ENEMY_EVENT:
                 enemy = Enemy()
                 self.enemy_group.add(enemy)
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            #     print("向右移动")
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+        # 使用键盘提供的方法 --> 可以动态的移动英雄飞机
+        keys_pressed = pygame.key.get_pressed()
+        # 判断元组中对应按键的索引
+        if (keys_pressed[pygame.K_RIGHT]
+                or keys_pressed[pygame.K_LEFT]
+                or keys_pressed[pygame.K_UP]
+                or keys_pressed[pygame.K_DOWN]):
+            if keys_pressed[pygame.K_RIGHT]:
+                self.hero.speed = 2
+            elif keys_pressed[pygame.K_LEFT]:
+                self.hero.speed = -2
+            elif keys_pressed[pygame.K_UP]:
+                self.hero.up_stats = True
+                self.hero.speed_d = -2
+            elif keys_pressed[pygame.K_DOWN]:
+                self.hero.up_stats = True
+                self.hero.speed_d = 2
+            else:
+                self.hero.speed = 0
+                self.hero.speed_d = 0
+                self.hero.up_stats = False
+                self.hero.co_key = False
+        else:
+            if keys_pressed[pygame.K_RIGHT] and keys_pressed[pygame.K_UP]:
+                self.hero.speed = 2
+                self.hero.up_stats = True
+                self.hero.speed_d = -2
+            elif keys_pressed[pygame.K_LEFT] and keys_pressed[pygame.K_DOWN]:
+                self.hero.speed = -2
+                self.hero.up_stats = True
+                self.hero.speed_d = 2
+            elif keys_pressed[pygame.K_RIGHT] and keys_pressed[pygame.K_DOWN]:
+                self.hero.speed = 2
+                self.hero.up_stats = True
+                self.hero.speed_d = 2
+            elif keys_pressed[pygame.K_LEFT] and keys_pressed[pygame.K_UP]:
+                self.hero.speed = -2
+                self.hero.up_stats = True
+                self.hero.speed_d = -2
+            else:
+                self.hero.speed = 0
+                self.hero.speed_d = 0
+                self.hero.up_stats = False
+                self.hero.co_key = False
 
     def __check_collide(self):
-        pass
+
+        # pygame.sprite.groupcollide(group1, group2,kill,kill,collide) --> sprite_list
+        # 两个精灵组中的精灵会被自动删除
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group,
+                                   True, True)
+        # pygame.sprite.spritecollide(sprite, group, dokill, collided=None) --> sprite_list
+        # 设置为True -- > 敌机会烧毁
+        enemier = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+        # 碰撞以后才会有内容
+        # 判断列表是否有内容
+        if len(enemier) > 0:
+            # 让英雄牺牲
+            self.hero.kill()
+            # 结束游戏
+            PlaneGame.__game_over()
 
     def __update_sprites(self):
 
@@ -73,6 +142,9 @@ class PlaneGame(object):
 
         self.hero_group.update()
         self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
--- Hei_Ma_Python/hei_ma_pygame/plane_sprites.py
+++ Hei_Ma_Python/hei_ma_pygame/plane_sprites.py
@@ -12,6 +12,11 @@ SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 FRAME_PER_SEC = 60
 # 敌机定时器常量
 CREATE_ENEMY_EVENT = pygame.USEREVENT
+# 英雄发射子弹时间
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
+
+DELAY = 100
+SWITCH_HERO = True
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -58,30 +63,110 @@ class Enemy(GameSprite):
     def __init__(self):
         # 确定敌机图片
         super().__init__("./images/enemy1.png")
-
+        # 设置敌机移动速度
         self.speed = random.randint(1, 3)
-
+        # 设置敌机从屏幕的顶部（0，0）出现
         self.rect.bottom = 0
-
+        # 设置敌机左右随机出现
         max_x = SCREEN_RECT.width - self.rect.width
         self.rect.x = random.randint(0, max_x)
 
+    # 监测敌机对象被销毁时的动作
     def __del__(self):
         # print("自毁")
         pass
 
     def update(self):
+        # 调用父类的方法
         super().update()
+        # 敌机飞出屏幕，从精灵组中删除精灵
         if self.rect.y >= SCREEN_RECT.height:
+            # kill方法可以直接把精灵删除
             self.kill()
 
 
 class Hero(GameSprite):
     """hero"""
 
-    def __init__(self):
+    def __init__(self, up_stats=False, co_key=False, speed_d=0):
 
         super().__init__("./images/me1.png", 0)
-
+        self.switch_hero = pygame.image.load("./images/me2.png")
+        # 设置英雄飞机的初始位置
         self.rect.centerx = SCREEN_RECT.centerx
+        # 在屏幕底部的上方120
         self.rect.bottom = SCREEN_RECT.bottom - 120
+        self.up_stats = up_stats
+        self.co_key = co_key
+        self.speed_d = speed_d
+
+        # 创建子弹精灵组
+        self.bullets = pygame.sprite.Group()
+
+    def update(self):
+        # 不需要调用父类的方法
+        # 更新hero
+        global SWITCH_HERO
+        global DELAY
+        # SWITCH_HERO = not SWITCH_HERO
+        if SWITCH_HERO:
+            self.image, self.switch_hero = self.switch_hero, self.image
+        # 更新hero的刷新帧率
+        if not(DELAY % 4):
+            SWITCH_HERO = not SWITCH_HERO
+        DELAY -= 1
+        if not DELAY:
+            DELAY = 100
+
+        if self.co_key:
+            if self.up_stats:
+                self.rect.y += self.speed_d
+            else:
+                self.rect.x += self.speed
+        else:
+            self.rect.y += self.speed_d
+            self.rect.x += self.speed
+        # 英雄在水平方向移动
+        # right -- max_x = SCREEN_RECT.width - self.rect.width
+        # bottom -- max_y = SCREEN_RECT.height - self.rect.height
+        # right = x + width
+        # left = x - width
+        if self.rect.x <= 0:
+            self.rect.x = 0
+        elif self.rect.right >= SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+        elif self.rect.y <= 0:
+            self.rect.y = 0
+        elif self.rect.bottom >= SCREEN_RECT.bottom:
+            self.rect.bottom = SCREEN_RECT.bottom
+        pass
+
+    def move_hero(self):
+        pass
+
+    def fire(self):
+
+        # 1. 创建子弹精灵
+        for i in (0, 1, 2):
+            bullet = Bullet()
+            # 设置精灵位置
+            bullet.rect.bottom = self.rect.y - 20*i
+            bullet.rect.centerx = self.rect.centerx
+            # 添加
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+    """子弹精灵"""
+    def __init__(self):
+        super().__init__("./images/bullet1.png", -2)
+
+    def update(self):
+        super().update()
+        # 判断子弹是否飞出屏幕
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        pass
+        # print("子弹被销毁")
